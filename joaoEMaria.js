8052c680b8b11843d8098170377f327ed343504f
Gianlucca Paiva <gianluccapaiva@gmail.com>
2025-02-27 09:17:27 -0300

    atividade 10

---
+++ joaoEMaria.js
@@ -0,0 +1,39 @@
+console.log('módulo joaoEMaria');
+
+let joao = 1;
+let maria = 2;
+
+function getMaria() {
+  return maria;
+}
+
+function getJoao() {
+    return joao;
+}
+
+function setJoao(novoValor) {
+    if(novoValor > 0){
+        joao = novoValor;
+    }else{
+        joao = 0;
+    }
+}
+
+function setMaria(novoValor) {
+    if(novoValor > 0){
+        maria = novoValor;
+    }else{
+        maria = 0;
+    }
+}
+
+function deJoaoParaMaria() {
+    maria = maria + joao;
+    joao = 0;
+}
+
+function deMariaParaJoao() {
+    joao = joao + maria;
+    maria = 0;
+}
+export { getMaria, getJoao, setJoao, setMaria, deJoaoParaMaria, deMariaParaJoao }
---
+++ main.js
@@ -0,0 +1,8 @@
+import { deJoaoParaMaria, getJoao, getMaria } from "./joaoEMaria";
+console.log('módulo main');
+
+console.log('João tem ',getJoao(), ' maçãs!');
+console.log('Maria tem ',getMaria(), ' maçãs!');
+deJoaoParaMaria();
+console.log('João tem ',getJoao(), ' maçãs!');
+console.log('Maria tem ',getMaria(), ' maçãs!');
